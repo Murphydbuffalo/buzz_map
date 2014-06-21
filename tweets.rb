bd15afc8facdf0875dcdb9e88aaa1bac11d38507
Dan Murphy <murphydbuffalo@gmail.com>
2014-06-20 20:46:51 -0400

    Extracted authentication and query processes into classes with single responsibility methods.

--- tweets.rb
+++ tweets.rb
@@ -3,82 +3,130 @@ require 'uri'
 require 'base64'
 require 'net/http'
 require 'dotenv'
-require 'rest_client'
-require 'cgi'
 require 'pry'
 
-Dotenv.load
+class TwitterAuthenticationRequest
+	Dotenv.load
+
+	attr_reader :http
+
+	def initialize(key, secret)
+		@key = key
+		@secret = secret
+	end
+
+	def uri_encode_key
+		URI(@key)
+	end
+
+	def uri_encode_secret
+		URI(@secret)
+	end
+
+	def base_64_encode_key_and_secret
+		auth = "#{uri_encode_key}:#{uri_encode_secret}"
+    "Basic #{Base64.strict_encode64(auth)}"
+	end
+
+	def auth_request_header
+    {
+			'authorization' => base_64_encode_key_and_secret,
+			'content_type' => 'application/x-www-form-urlencoded;charset=UTF-8',
+		  'grant_type' => 'grant_type=client_credentials'
+	  }
+	end
+
+	def auth_request_url
+		URI.parse('https://api.twitter.com/oauth2/token')
+	end
+
+	def create_auth_http_object
+		@http = Net::HTTP.new(auth_request_url.host, auth_request_url.port)
+		@http.use_ssl = true
+		@http
+	end
+
+	def create_auth_request
+    req = Net::HTTP::Post.new(auth_request_url.path)
+		req.add_field('Authorization', auth_request_header['authorization'])
+		req.add_field('Content-Type', auth_request_header['content_type'])
+		req.body = auth_request_header['grant_type']
+		req
+	end
+
+	def make_auth_request
+		create_auth_http_object.request(create_auth_request)
+	end
+
+	def get_bearer_token
+		JSON.parse(make_auth_request.body)["access_token"]
+	end
+	 
+end
+
+class TwitterQuery
+	
+	def initialize(query, base_url, parameters)
+		@base_url = base_url
+		@query = query
+		@parameters = parameters
+		@tweets = []
+		@locations = []
+		@retweet_locations = []
+		@since_id = 0
+	end
+
+	def uri_encode_query_url
+		URI.parse("#{@base_url}#{@query}#{@parameters}#{@since_id}")
+		#Since_id is the lowest tweet id number your query will retrieve.
+		#You can set since_id equal to the highest tweet id number you've received.
+		#This ensures that you only retrieve new tweets as you make more requests (eg, within the loop below). 
+	end
+
+	def create_query_http_object
+		@query_http = Net::HTTP.new(uri_encode_query_url.host, uri_encode_query_url.port)
+	  @query_http.use_ssl = true
+	end
+
+	def create_query_request
+		query_request = Net::HTTP::Get.new(uri_encode_query_url.path)
+		query_request.add_field('Authorization', "Bearer #{TwitterAuthenticationRequest.bearer_token}")
+		query_request
+	end
+
+	def get_tweets
+		until @tweets.count >= 1000
+			ids = []
+			query_response = JSON.parse(create_query_http_object.request(@query_http).body)
+			@tweets += query_response["statuses"]
+			@tweets.each {|tweet| ids << tweet["id"]}
+			@since_id = ids.max
+		end
+	end
+
+	def get_locations
+		@tweets.each do |tweet|
+  		@locations << tweet["user"]["location"] if tweet["user"]["location"] != nil
+  		@retweet_locations << tweet["retweeted_status"]["user"]["location"] if tweet["retweeted"] == true && tweet["retweeted_status"] != nil
+		end
+		#tweet['retweeted_status']['user']['location'] is the correct path
+	end
+		
+end
 
 api_key = ENV['TWITTER_API_KEY']
 api_secret = ENV['TWITTER_SECRET_KEY']
 
-api_key_url = URI(api_key)
-api_secret_url = URI(api_secret)
+auth_request = TwitterAuthenticationRequest.new(ENV['TWITTER_API_KEY'], ENV['TWITTER_SECRET_KEY'])
+bearer_token = auth_request.get_bearer_token
+puts bearer_token
 
-app_only_auth = api_key_url.to_s + ":" + api_secret_url.to_s
+#base_url = "https://api.twitter.com/1.1/search/tweets.json"
+#&result_type=popular
+#query = "q=#ruby%20or%20rails"
+#parameters = "&count=100&since_id=#{count}"
 
-authorization = "Basic #{Base64.strict_encode64(app_only_auth)}"
-
-url = URI.parse('https://api.twitter.com/oauth2/token')
-
-content_type = 'application/x-www-form-urlencoded;charset=UTF-8'
-
-grant_type = 'grant_type=client_credentials'
-
-header = {
-	'authorization' => authorization,
-	'content_type' => content_type,
-  'grant_type' => grant_type
-  }
-
-http = Net::HTTP.new(url.host, url.port)
-http.use_ssl = true
-
-req = Net::HTTP::Post.new(url.path)
-req.add_field("Authorization", authorization)
-req.add_field("Content-Type", content_type)
-req.body = grant_type
-response = http.request(req)
-
-parsed_response = JSON.parse(response.body)
-bearer_token = parsed_response["access_token"]
-
-query = "ruby%20or%20rails"
-
-count = 0
-
-api_url = "https://api.twitter.com/1.1/search/tweets.json?q=#{query}&count=100$since_id=#{count}"
-
-get_url = URI.parse(api_url)
-
-get_http = Net::HTTP.new(get_url.host, get_url.port)
-get_http.use_ssl = true
-
-get_request = Net::HTTP::Get.new(get_url)
-get_request.add_field('Authorization', "Bearer #{bearer_token}")
-
-tweets = []
-coordinates = []
-ids = []
-
-until tweets.count >= 1000
-	ids = []
-	
-	get_response = http.request(get_request)
-	parsed_get_response = JSON.parse(get_response.body)
-	tweets += parsed_get_response["statuses"]
-	
-	tweets.each {|tweet| ids << tweet["id"]}
-	count = ids.max
-end
-
-tweets.each do |tweet|
-    coordinates << tweet["coordinates"] if tweet["coordinates"] != nil
-    coordinates << tweet["retweeted_status"]["coordinates"] if tweet["retweeted"] != false 
-end
-
-puts tweets.count
-
-puts coordinates.count
-
-#binding.pry
+# puts tweets.count
+# puts locations.count
+# puts retweet_locations.count
+# binding.pry
