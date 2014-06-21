60a2af89860a677482265f1076dd11ad46ac9fcf
Dan Murphy <murphydbuffalo@gmail.com>
2014-06-21 19:04:04 -0400

    Consolidated similar methods for creating different variations of HTTP requests.

--- tweets.rb
+++ tweets.rb
@@ -5,74 +5,36 @@ require 'net/http'
 require 'dotenv'
 require 'pry'
 
-class TwitterAuthentication
+class TwitterRequest
 	Dotenv.load
 
-	attr_reader :url, :key, :secret
+	attr_reader :url, :key, :secret, :headers, :body
 
 	def initialize(url, key, secret)
 		@url = url
 		@key = key
 		@secret = secret
-	end
-
-	def get_bearer_token
-		JSON.parse(make_auth_request.body)["access_token"]
-	end
-
-	protected
-
-	def uri_encode(url)
-		URI.parse(url)
+		@since_id = 0
 	end
 
 	def base_64_encode_key_and_secret
-		auth = "#{uri_encode(key)}:#{uri_encode(secret)}"
-    "Basic #{Base64.strict_encode64(auth)}"
+    "Basic #{Base64.strict_encode64("#{uri_encode(key)}:#{uri_encode(secret)}")}"
 	end
 
-	def auth_request_content
-    {
-			'authorization' => base_64_encode_key_and_secret,
-			'content_type' => 'application/x-www-form-urlencoded;charset=UTF-8',
-		  'grant_type' => 'grant_type=client_credentials'
-	  }
+	def add_request_content(headers, body)
+    @headers = headers
+    @body = body
 	end
 
-	def create_auth_http_object
-		http = Net::HTTP.new(uri_encode(url).host, uri_encode(url).port)
-		http.use_ssl = true
-		http
-	end
-
-	def create_auth_request
-    req = Net::HTTP::Post.new(uri_encode(url).path)
-		req.add_field('Authorization', auth_request_content['authorization'])
-		req.add_field('Content-Type', auth_request_content['content_type'])
-		req.body = auth_request_content['grant_type']
-		req
-	end
-
-	def make_auth_request
-		create_auth_http_object.request(create_auth_request)
-	end
-	 
-end
-
-class TwitterQuery
-	
-	def initialize(base_url, query, parameters)
-		@base_url = base_url
-		@query = query
-		@since_id = 0
-		@parameters = parameters
+	def get_bearer_token(request_type)
+		JSON.parse(make_request(request_type).body)["access_token"]
 	end
 
 	def get_tweets(bearer_token)
 		tweets = []
 		until tweets.count >= 1000
 			ids = []
-			@query_response = JSON.parse(make_query_request(bearer_token).body)
+			@query_response = JSON.parse(make_request('Get').body)
 			tweets += @query_response["statuses"]
 			tweets.each {|tweet| ids << tweet["id"]}
 			@since_id = ids.max
@@ -93,36 +55,41 @@ class TwitterQuery
 
 	protected
 
-	def uri_encode_query_url
-		URI.parse("#{@base_url}#{@query}#{@parameters}#{@since_id}")
+	def uri_encode(url)
+		URI.parse(url)
+	end
+	
+	def create_http_object
+		http = Net::HTTP.new(uri_encode(url).host, uri_encode(url).port)
+		http.use_ssl = true
+		http
 	end
 
-	def create_query_http_object
-	  query_http = Net::HTTP.new(uri_encode_query_url.host, uri_encode_query_url.port)
-	  query_http.use_ssl = true
-	  query_http
+	def create_request(type)
+		type.downcase.start_with?('p') ? req = Net::HTTP::Post.new(uri_encode(url).path) : req = Net::HTTP::Get.new(uri_encode(url).path)
+		headers.each {|k, v| req.add_field(k, v) } 
+		req.body = body
+		req
 	end
 
-	def create_query_request(bearer_token)
-		query_request = Net::HTTP::Get.new(uri_encode_query_url.path)
-		query_request.add_field('Authorization', "Bearer #{bearer_token}")
-		query_request
+	def make_request(type)
+		create_http_object.request(create_request(type))
 	end
-
-	def make_query_request(bearer_token)
-		create_query_http_object.request(create_query_request(bearer_token))
-	end
-		
+	 
 end
-#CAN MAKE ONE 'ENCODE' METHOD THAT TAKES THE STRING TO ENCODE AND MODULE TO USE
-#CAN MAKE ONE METHOD THAT CREATES HTTP OBJECTS OF THE APPROPRIATE TYPE (OPTIONAL HTTP METHOD ARG)
-#AND ANOTHER TO ADD THE NEEDED HEADER AND BODY CONTENT
-#SHARE THESE METHODS FOR GETTING THE BEARER TOKEN AND THE JSON DATA
 
-bearer_token = TwitterAuthentication.new('https://api.twitter.com/oauth2/token', ENV['TWITTER_API_KEY'], ENV['TWITTER_SECRET_KEY']).get_bearer_token
+bearer_token_request = TwitterRequest.new('https://api.twitter.com/oauth2/token', ENV['TWITTER_API_KEY'], ENV['TWITTER_SECRET_KEY'])
+auth = bearer_token_request.base_64_encode_key_and_secret
+bearer_token_request.add_request_content(
+	{'authorization' => auth, 'content_type' => 'application/x-www-form-urlencoded;charset=UTF-8'},
+	'grant_type=client_credentials')
 
-query_request = TwitterQuery.new("https://api.twitter.com/1.1/search/tweets.json", "?q=#ruby%20or%20rails", "&count=100&since_id=#{@since_id}")
-#&result_type=popular
+bearer_token = bearer_token_request.get_bearer_token('Post')
+
+query_request = TwitterRequest.new("https://api.twitter.com/1.1/search/tweets.json?q=#ruby&count=100", ENV['TWITTER_API_KEY'], ENV['TWITTER_SECRET_KEY'])
+query_request.add_request_content({'Authorization' => "Bearer #{bearer_token}"}, '')
+#&since_id=#{@since_id} &result_type=popular
 binding.pry
-tweets = query_request.get_tweets(bearer_token)
+tweets = query_request.get_tweets
 locations = query_request.get_locations
+binding.pry
