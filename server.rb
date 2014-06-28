a068f599a32851e70fc52df233ca0c1e097bafaf
David Guerette <david.guerette@gmail.com>
2014-06-28 15:19:17 -0400

    County name strings parsed correctly for SVG map string format.

--- server.rb
+++ server.rb
@@ -8,15 +8,10 @@ require 'dotenv'
 
 Dotenv.load
 
-def get_tweet_data
-  #fetch tweet data: lat, long, twitter user_name, corresponding tweet id
-end
-
-
 def get_counties_from_coordinates(lat,long)
   api_key = ENV['MAPS_ACCESS_KEY']
 
-  api_url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=#{lat},#{long}&result_type=administrative_area_level_1&key=#{api_key}"
+  api_url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=#{lat},#{long}&result_type=administrative_area_level_2&key=#{api_key}"
   get_url = URI.parse(api_url)
 
   get_http = Net::HTTP.new(get_url.host, get_url.port)
@@ -26,29 +21,27 @@ def get_counties_from_coordinates(lat,long)
   get_response = get_http.request(get_request)
 
   parsed_get_response = JSON.parse(get_response.body)
-
 end
 
 def create_counties_array
   counties = []
-  #call get_tweet_data
-  coordinates_array = [[40.714224, -73.961452], [45.5200, 122.6819], [42.3581, 71.0636]] #replace with live twitter data
 
+  coordinates_array = [[40.714224, -73.961452],[41.8819, 87.6278], [42.3581, 71.0636], [39.0997, 94.5783]] #replace with live twitter data
   coordinates_array.each do |county_coordinates|
-    # county = ''
     lat = county_coordinates[0].to_s
     long = county_coordinates[1].to_s
-
     data = get_counties_from_coordinates(lat, long)
-      if data["results"][0]["formatted_address"] != nil
-        county = data["results"][0]["formatted_address"].split
 
-        delete_words = ['County,', 'USA']
-        county = county.delete_if {|x| delete_words.include?(x)}.join(' ').gsub(',', '').split
-        county[-2] = county[-2] + ","
-        county = county.join(' ').to_s
+      if data["status"] != "ZERO_RESULTS"
+        county = data["results"][0]["formatted_address"] # STARTING FORMAT: "Kings County, NY, USA"
 
-        counties.push(county)
+        county_array = county.gsub(',', '').split
+        county_array.pop
+        county_array.delete_at(-2)
+        formatted_county_name = county_array.join(' ')
+        formatted_county_name.insert(-4, ',')
+
+        counties.push(formatted_county_name)
       end
     end
   counties
@@ -61,7 +54,7 @@ get '/' do
 end
 
 get '/test' do
-  # api_key = ENV['MAPS_ACCESS_KEY']
   @data = create_counties_array
+  binding.pry
   erb :'test.html'
 end
