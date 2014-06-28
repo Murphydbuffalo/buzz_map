e3fbcae7ac110329db528db41eb44b564554c35d
David Guerette <david.guerette@gmail.com>
2014-06-28 12:48:42 -0400

    Google Maps API env connection established.

--- server.rb
+++ server.rb
@@ -1,15 +1,24 @@
 require 'sinatra'
 require 'pg'
-
 require 'pry'
 require 'json'
 require 'uri'
 require 'net/http'
+require 'dotenv'
 
-def encode_url(lat,long)
-  api_url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=#{lat},#{long}&result_type=administrative_area_level_1&key=AIzaSyBh0PwMktnnDgOYyyY993LnXcUnv-D6Zcg"
+Dotenv.load
+
+def get_tweet_data
+  #fetch tweet data: lat, long, twitter user_name, corresponding tweet id
+end
+
+
+def get_coordinates_from_tweets(lat,long)
+  api_key = ENV['MAPS_ACCESS_KEY']
+
+  api_url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=#{lat},#{long}&result_type=administrative_area_level_1&key=#{api_key}"
   get_url = URI.parse(api_url)
-
+binding.pry
   get_http = Net::HTTP.new(get_url.host, get_url.port)
   get_http.use_ssl = true
 
@@ -17,43 +26,39 @@ def encode_url(lat,long)
   get_response = get_http.request(get_request)
 
   parsed_get_response = JSON.parse(get_response.body)
-
 end
 
-def get_counties
-
+def get_counties_from_coordinates
   counties = []
-
-  coordinates_array = [[42.3581, 71.0636], [37.7833, 122.4167], [45.5200, 122.6819]]
+  #call get coordinates_from_tweets
+  coordinates_array = [[42.3581, 71.0636], [37.7833, 122.4167], [45.5200, 122.6819]] #replace with live twitter data
 
   coordinates_array.each do |county_coordinates|
     lat = county_coordinates[0].to_s
     long = county_coordinates[1].to_s
 
-    data = encode_url(lat, long)
+    data = get_coordinates_from_tweets(lat, long)
 
     county = data["results"][0]["formatted_address"].split
-
     delete_words = ['County,', 'USA']
     county = county.delete_if {|x| delete_words.include?(x)}.join(' ').gsub(',', '').split
     county[-2] = county[-2] + ","
     county = county.join(' ').to_s
 
     counties.push(county)
-
   end
-
   counties
-
-
 end
 
 get '/' do
-
   # @counties = get_counties
-
   @test = 'testing!'
-
   erb :'index.html'
 end
 
+get '/test' do
+  # api_key = ENV['MAPS_ACCESS_KEY']
+  # binding.pry
+  @data = get_counties_from_coordinates
+  erb :'test.html'
+end
